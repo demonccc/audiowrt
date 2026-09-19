072d57422fd0a4612a2d6a2b93f652ad8bebb19c
Claudio Cesar Sanchez Tejeda <demonccc.y@gmail.com>
2026-09-19 14:00:23 -0300

    test: remove obsolete custom SBC and BlueZ library build roots

--- tests/test-package-build-selection.sh
+++ tests/test-package-build-selection.sh
@@ -25,8 +25,6 @@ audiowrt-wifi-client|package/feeds/audiowrt/audiowrt-wifi-client/compile
 luci-app-audiowrt-wifi-client|package/feeds/audiowrt/luci-app-audiowrt-wifi-client/compile
 audiowrt-spotify|package/feeds/audiowrt/audiowrt-spotify/compile
 librespot|package/feeds/audiowrt/librespot/compile
-audiowrt-sbc|package/feeds/audiowrt/audiowrt-sbc/compile
-audiowrt-bluez-libs|package/feeds/audiowrt/audiowrt-bluez/compile
 audiowrt-bluez|package/feeds/audiowrt/audiowrt-bluez/compile
 audiowrt-btctl|package/feeds/audiowrt/audiowrt-btctl/compile
 bluez-alsa|package/feeds/audiowrt/bluez-alsa/compile
@@ -61,19 +59,15 @@ Package: audiowrt-spotify
 Depends: +audiowrt-extensions +librespot
 Package: librespot
 Depends: +alsa-lib
-Package: audiowrt-sbc
-Depends: +libc
-Package: audiowrt-bluez-libs
-Depends: +libpthread
 Package: audiowrt-bluez
-Depends: +audiowrt-bluez-libs +glib2 +dbus +alsa-lib
+Depends: +bluez-libs +glib2 +dbus +alsa-lib
 Package: audiowrt-minimal-alsa
 Depends: +kmod-sound-core
 Provides: alsa-lib
 Package: audiowrt-btctl
 Depends: +glib2
 Package: bluez-alsa
-Depends: +alsa-lib +audiowrt-bluez +audiowrt-bluez-libs +glib2 +audiowrt-sbc +dbus
+Depends: +alsa-lib +audiowrt-bluez +bluez-libs +glib2 +sbc +dbus
 Package: audiowrt-bluetooth
 Depends: +audiowrt-audio +audiowrt-bluez +audiowrt-btctl +bluez-alsa +kmod-bluetooth +kmod-btusb
 Package: kmod-audiowrt-bluetooth
@@ -98,7 +92,7 @@ grep -q '^audiowrt-busybox|' "$tmp/core"
 grep -q '^audiowrt-wifi-client|' "$tmp/core"
 grep -q '^luci-app-audiowrt-wifi-client|' "$tmp/core"
 grep -q '^audiowrt-extensions|' "$tmp/core"
-if grep -Eq '^(audiowrt-storage|audiowrt-storage-luci|audiowrt-spotify|librespot|audiowrt-bluetooth|bluez-alsa|audiowrt-bluez|audiowrt-bluez-libs|audiowrt-sbc|audiowrt-btctl)\|' "$tmp/core"; then
+if grep -Eq '^(audiowrt-storage|audiowrt-storage-luci|audiowrt-spotify|librespot|audiowrt-bluetooth|bluez-alsa|audiowrt-bluez|audiowrt-btctl)\|' "$tmp/core"; then
     echo "ERROR: non-Bluetooth core roots selected unrelated AudioWRT packages." >&2
     cat "$tmp/core" >&2
     exit 1
@@ -113,7 +107,7 @@ python3 "$resolver" "$tmp/targets" "$tmp/packageinfo" \
     audiowrt-core audiowrt-extensions audiowrt-spotify > "$tmp/spotify"
 grep -q '^librespot|' "$tmp/spotify"
 grep -q '^audiowrt-spotify|' "$tmp/spotify"
-if grep -Eq '^(audiowrt-bluetooth|bluez-alsa|audiowrt-bluez|audiowrt-bluez-libs|audiowrt-sbc|audiowrt-btctl)\|' "$tmp/spotify"; then
+if grep -Eq '^(audiowrt-bluetooth|bluez-alsa|audiowrt-bluez|audiowrt-btctl)\|' "$tmp/spotify"; then
     echo "ERROR: Spotify build selected Bluetooth packages." >&2
     exit 1
 fi
@@ -124,7 +118,7 @@ fi
 python3 "$resolver" "$tmp/targets" "$tmp/packageinfo" \
     audiowrt-core audiowrt-extensions audiowrt-minimal-alsa kmod-audiowrt-bluetooth \
     kmod-audiowrt-sound-core kmod-audiowrt-usb-audio audiowrt-bluetooth > "$tmp/bluetooth"
-for package in audiowrt-minimal-alsa audiowrt-sbc audiowrt-bluez-libs audiowrt-bluez audiowrt-btctl bluez-alsa kmod-audiowrt-bluetooth audiowrt-bluetooth; do
+for package in audiowrt-minimal-alsa audiowrt-bluez audiowrt-btctl bluez-alsa kmod-audiowrt-bluetooth audiowrt-bluetooth; do
     grep -q "^${package}|" "$tmp/bluetooth"
 done
 for package in kmod-audiowrt-sound-core kmod-audiowrt-usb-audio; do
